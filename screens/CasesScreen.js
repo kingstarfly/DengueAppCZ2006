dafe3a3da4d63a9be818bfdcfad1a621bd8b5b6b
xx <xingxiang@twotreesgroup.com>
2020-10-05 22:43:26 +0800

    fiddled with tab nav bar

--- screens/CasesScreen.js
+++ screens/CasesScreen.js
@@ -12,14 +12,38 @@ const Tab = createMaterialTopTabNavigator();
 const CasesScreen = ({ navigation }) => {
   return (
     <Tab.Navigator
+      tabBarOptions={{
+        labelStyle: {
+          fontSize: 14,
+          textTransform: "capitalize",
+        },
+
+        indicatorStyle: {
+          width: 10,
+          height: 10,
+          marginLeft: 100,
+          marginBottom: 2,
+          borderRadius: 10,
+          marginHorizontal: "auto",
+        },
+      }}
       initialRouteName="SpecificLocation"
       screenOptions={{ headerShown: false }}
       backBehavior={"none"}
     >
-      <Tab.Screen name="SpecificLocation" component={SpecificLocationScreen} />
+      <Tab.Screen
+        name="SpecificLocation"
+        component={SpecificLocationScreen}
+        options={{
+          title: "Your Location",
+        }}
+      />
       <Tab.Screen
         name="SingaporeLocation"
         component={SingaporeLocationScreen}
+        options={{
+          title: "Singapore",
+        }}
       />
     </Tab.Navigator>
   );
